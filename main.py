248434e198ce108a7a0fcbd37ec24c851e0ad8be
ddrddrr <asanikor@bk.ru>
2022-10-06 17:40:44 +0200

    Added scalability
    Rewritten functions in main in a way that they right now can be
    applied to any website, using constants that are associated with
    these websites.
    Info-gathering(previous gather_info() function) is now separate for
    every website.
    Rewritten accep_cookies and make_search, now they do not need
    button_click_script as it is done inside the function itself.
    accept_cookies now can skip accepting if param cookie_button is None

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import importlib
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -5,30 +6,42 @@ from selenium.webdriver.chrome.options import Options
 from time import sleep
 
 
+# All pages are in form of "https://www.NAME.domain"
+def strip_webpage_name(web_adress: str):
+    return web_adress[web_adress.find(".") + 1: web_adress.rfind(".")]
+
+
 def search_elements(driver, elem_name, by_id):
     if by_id:
         return driver.find_elements(By.ID, elem_name)
-    return driver.find_elements(By.CLASS_NAME, elem_name)
+    return driver.find_elements(By.XPATH, f"//*[@class='{elem_name}']")  # TODO tests
 
 
-def accept_cookies(driver, cookie_button, by_id, accept_script) -> bool:
-    if len(search_elements(driver, cookie_button, by_id)) != 0:
+def accept_cookies(driver, cookie_button: tuple[str, bool], accept_script) -> bool:
+    if cookie_button is None:
+        return True
+
+    button = cookie_button[0]
+    by_id = cookie_button[1]
+    actual_button = search_elements(driver, button, by_id)
+    if len(actual_button) != 0:
         print("Found cookie button")
-        driver.execute_script(accept_script)
+        actual_button[0].click()
+        # driver.execute_script(accept_script)
         return True
 
     print("Could not find cookie button")
     return False
 
 
-def make_search(driver, query: str, search_button: str, button_by_id: bool,
-                search_field: str, field_by_id: bool, button_click_script: str) -> bool:
-    input_field = search_elements(driver, search_button, button_by_id)
-
+def make_search(driver, query: str, search_button: tuple[str, bool],
+                search_field: tuple[str, bool], button_click_script: str) -> bool:
+    input_field = search_elements(driver, search_button[0], search_button[1])
     if len(input_field) != 0:
         print("Trying to search")
-        driver.execute_script(button_click_script)
-        input_field = search_elements(driver, search_field, field_by_id)
+        # driver.execute_script(button_click_script)
+        input_field[0].click()
+        input_field = search_elements(driver, search_field[0], search_field[1])
         if len(input_field) != 0:
             input_field[0].send_keys(query)
             input_field[0].send_keys(Keys.ENTER)
@@ -48,59 +61,49 @@ def sort_items_on_page(driver, scripts):
         driver.execute_script(script)
 
 
-def gather_info(driver):
-    all_items = driver.find_elements(By.CLASS_NAME, "itemInformation")
-    if len(all_items) == 0:
-        print("Could not gather info about items")
-        return -1
-
-    for i, item in enumerate(all_items):
-        name = item.find_elements(By.CLASS_NAME, "itemTitle")[0].find_elements(By.TAG_NAME, "a")[0].text
-        if not name:
-            print(f"Could not find name of item {i + 1}")
-
-        price = item.find_elements(By.CLASS_NAME, "itemPrice")[0]. \
-            find_elements(By.CLASS_NAME, "pri")[0]
-        price_with_sale = price.find_elements(By.CLASS_NAME, "now")
-        if len(price_with_sale) != 0:
-            price = price_with_sale[0]
-
-        if not price:
-            print(f"Could not find price of item {i + 1}")
-
-        if name and price:
-            print(f"{i + 1} ", name, price.text)
-
-    return 0
-
-
 def main():
     chrome_options = Options()
     chrome_options.add_experimental_option("detach", True)
     chrome_options.add_argument("start-maximized")
     driver = webdriver.Chrome(executable_path="C:\\progy\\chromedriver.exe", options=chrome_options)
-    curr_website = "https://www.footpatrol.com"
+
+    pages = ["https://www.footlocker.cz", "https://www.footpatrol.com"]
     query = "Nike Air Force"
-    driver.get(curr_website)
 
-    if not accept_cookies(driver, "accept-all-cookies", False,
-                          "document.getElementsByClassName(\"btn btn-level1 accept-all-cookies\")[0]"
-                          ".dispatchEvent(new Event(\"click\"))"):
-        return -1
-    sleep(1)
+    for page in pages:
+        driver.get(page)
 
-    if not make_search(driver, query, "searchButton", True, "srchInput", True,
-                       "document.getElementById(\"searchButton\")"
-                       ".dispatchEvent(new Event(\"click\"))"):
-        return -1
-    sleep(1)
+        module_name = strip_webpage_name(page)
+        curr_site_module = importlib.import_module(module_name) #TODO think of a better way
+        sleep(1)
 
-    sort_items_on_page(driver, ["document.querySelector('[value=price-low-high]').selected = true",
-                                "document.getElementById(\"sortFormTop\").dispatchEvent(new Event(\"submit\"))"])
-    sleep(1)
+        if not accept_cookies(driver, curr_site_module.cookie_button, curr_site_module.cookie_accept_script):
+            return -1
+        sleep(1)
 
-    return gather_info(driver)
+        if not make_search(driver, query, curr_site_module.search_button, curr_site_module.search_field,
+                           curr_site_module.search_button_click_script):
+            return -1
+        sleep(1)
+
+        sort_items_on_page(driver, curr_site_module.price_sort_scripts)
+        sleep(1)
+
+        return curr_site_module.gather_info(driver)
+
+
+def test_footlocker():
+    chrome_options = Options()
+    chrome_options.add_experimental_option("detach", True)
+    chrome_options.add_argument("start-maximized")
+    driver = webdriver.Chrome(executable_path="C:\\progy\\chromedriver.exe", options=chrome_options)
+    driver.get("https://www.footlocker.cz")
+    sleep(4)
+    make_search(driver, "New Balance 574", ("HeaderSearch_search_query", True), ("HeaderSearch_search_query", True),
+                "document.getElementsByClassName(\"SearchForm-button\")[0]"
+                ".dispatchEvent(new Event(\"click\"))")
 
 
 if __name__ == '__main__':
-    main()
+    # main()
+    test_footlocker()
